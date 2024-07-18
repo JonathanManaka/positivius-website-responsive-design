18cb8348435e664cfb32944fbf26bbbab22abb4f
JonathanManaka <mphomanaka91@gmail.com>
2024-07-18 13:48:03 +0200

    added script in a new file for steps

---
+++ scripts/toogle-fucn.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  $("#toogle-div").toggle();
+  $("#step1-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn").click(function(){
+       $("#toogle-div").toggle();
+       if($("#toogle-div").is(":hidden")){
+          $("#step1-id-container").css("background-color", "#FFFFFF")
+          $("#toggle-icon").attr("src","assets/icons/plus-icon.svg")
+       }else{
+            $("#step1-id-container").css("background-color", "#B9FF66")
+            $("#toggle-icon").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
+
+  $(document).ready(function(){
+    $("#toogle-div-2").toggle();
+    $("#step2-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn2").click(function(){
+       $("#toogle-div-2").toggle();
+       if($("#toogle-div-2").is(":hidden")){
+        $("#step2-id-container").css("background-color", "#FFFFFF")
+         $("#toggle-icon2").attr("src","assets/icons/plus-icon.svg")
+       }else{
+        $("#step2-id-container").css("background-color", "#B9FF66")
+         $("#toggle-icon2").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
+
+  $(document).ready(function(){
+    $("#toogle-div-3").toggle();
+    $("#step3-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn3").click(function(){
+       $("#toogle-div-3").toggle();
+       if($("#toogle-div-3").is(":hidden")){
+        $("#step3-id-container").css("background-color", "#FFFFFF")
+         $("#toggle-icon3").attr("src","assets/icons/plus-icon.svg")
+       }else{
+        $("#step3-id-container").css("background-color", "#B9FF66")
+         $("#toggle-icon3").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
+
+  $(document).ready(function(){
+    $("#toogle-div-4").toggle();
+    $("#step4-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn4").click(function(){
+       $("#toogle-div-4").toggle();
+       if($("#toogle-div-4").is(":hidden")){
+        $("#step4-id-container").css("background-color", "#FFFFFF")
+         $("#toggle-icon4").attr("src","assets/icons/plus-icon.svg")
+       }else{
+        $("#step4-id-container").css("background-color", "#B9FF66")
+         $("#toggle-icon4").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
+
+  $(document).ready(function(){
+    $("#toogle-div-5").toggle();
+    $("#step5-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn5").click(function(){
+       $("#toogle-div-5").toggle();
+       if($("#toogle-div-5").is(":hidden")){
+        $("#step5-id-container").css("background-color", "#FFFFFF")
+         $("#toggle-icon5").attr("src","assets/icons/plus-icon.svg")
+       }else{
+        $("#step5-id-container").css("background-color", "#B9FF66")
+         $("#toggle-icon5").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
+
+  $(document).ready(function(){
+    $("#toogle-div-6").toggle();
+    $("#step6-id-container").css("background-color", "#FFFFFF")
+    $("#show-hide-btn6").click(function(){
+       $("#toogle-div-6").toggle();
+       if($("#toogle-div-6").is(":hidden")){
+        $("#step6-id-container").css("background-color", "#FFFFFF")
+         $("#toggle-icon6").attr("src","assets/icons/plus-icon.svg")
+       }else{
+        $("#step6-id-container").css("background-color", "#B9FF66")
+         $("#toggle-icon6").attr("src","assets/icons/minus-icon.svg")
+       }
+    });
+  });
